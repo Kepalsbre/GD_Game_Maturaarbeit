9f1a3c4b5d9b3cd9d800251310fa1847b2f69f56
Kepalsbre <128705198+Kepalsbre@users.noreply.github.com>
2024-06-03 21:57:35 +0200

    reset

--- AbilityGame/scripts/player.gd
+++ AbilityGame/scripts/player.gd
@@ -22,9 +22,6 @@ func _process(delta):
 		dash_velocity = velocity
 		$DashTimer.start()
 		$DashParticles.emitting = true
-		
-	
-	#
 	
 	# normalize vector to prevent inconsistent movement and apply speed
 	if velocity.length() > 0:
