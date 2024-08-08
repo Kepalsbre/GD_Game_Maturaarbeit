950202b20aef410eac577280725d87a4af2fdba9
Kepalsbre <128705198+Kepalsbre@users.noreply.github.com>
2024-08-08 21:19:41 +0200

    added slinger enemy

---
+++ AbilityGame/scripts/enemy_bullet.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var damage := 10.0
+var knockback := 0.0
+var speed := 1400
+var velocity := Vector2.ZERO
+
+func _on_hitbox_component_area_entered(area):
+	if area is HitboxComponent:
+		var hitbox : HitboxComponent = area
+		var attack = Attack.new()
+		attack.attack_damage = damage
+		attack.knockback_force = knockback
+		attack.attack_position = global_position
+		hitbox.damage(attack)
+		queue_free()
+
+func _physics_process(delta):
+	global_position += velocity * delta * speed
+
+func _on_lifetime_timeout():
+	queue_free()
--- AbilityGame/scripts/slinger_enemy.gd
+++ AbilityGame/scripts/slinger_enemy.gd
@@ -2,11 +2,12 @@ extends CharacterBody2D
 
 enum state {idle, seek, attack}
 @export var current_state: state = state.idle
-@export var wake_lenght := 600
-@export var knockback: float = 1.2:
+@export var wake_lenght := 400
+
+@export var knockback: float = 1.0:
 	get:
 		return knockback * Global.enemy_knockback_multiplier
-@export var damage: int = 10:
+@export var damage: int = 15:
 	get:
 		return damage * Global.enemy_dmg_multiplier
 
@@ -15,6 +16,10 @@ enum state {idle, seek, attack}
 @onready var offset_timer = $OffsetTimer
 @onready var health_component = $HealthComponent
 @onready var navigation_agent_2d = $NavigationAgent2D
+@onready var animation_player = $AnimationPlayer
+const ENEMY_BULLET = preload("res://scenes/enemy_bullet.tscn")
+@onready var enemy_projectile_image = $EnemyProjectileImage
+@onready var hp_bar_component = $HpBarComponent
 
 
 var player_pos
@@ -32,7 +37,13 @@ func _ready():
 		enemy.wake_up.connect(_on_wake_up)
 	health_component.max_health *= Global.enemy_hp_multiplier
 	health_component.health *= Global.enemy_hp_multiplier
+	hp_bar_component.top_level = true
+	hp_bar_component.visible = false
 	
+	
+func _process(_delta):
+	hp_bar_component.position = global_position + Vector2(-61, 85)
+
 func _physics_process(delta):
 	player_pos = Global.player_pos
 	match current_state:
@@ -57,13 +68,17 @@ func _physics_process(delta):
 			if position.distance_to(player_pos) > 1100:
 				animations.play("follow")
 				current_state = state.seek
-	
+				animation_player.stop()
+				
+	# knockback stuff
 	if knock_frames != 0:
 		knock_frames -= 1
 		velocity = knockback_received
 	else:
 		knockback_received = Vector2.ZERO
-	look_at(player_pos)
+	if current_state != state.idle:
+		look_at(player_pos)
+		
 	move_and_collide(velocity*delta)
 	
 
@@ -72,25 +87,33 @@ func randoffset():
 	var distance = randi_range(30,200)
 	return Vector2(distance * cos(angle), distance * sin(angle)) # polar to cartesian
 
-
 func _on_offset_timer_timeout():
 	offset = randoffset()
-
-
-
 
 func knock_back(knockforce, knock_pos):
 	knockback_received = (global_position - knock_pos) 
 	knockback_received = knockback_received.normalized() * (speed / 4) * knockforce
-
 	knock_frames = 20
-	
 	
 func _on_wake_up():
 	await get_tree().create_timer(randf_range(1,15)).timeout
 	awake = true
 	
-
 func _on_navigation_agent_2d_target_reached():
 	animations.play("attack")
+	animation_player.play("shoot")
 	current_state = state.attack
+
+func create_bullet(spawn_pos):
+	var new_bullet = ENEMY_BULLET.instantiate()
+	new_bullet.damage = damage
+	new_bullet.knockback = knockback
+	new_bullet.velocity = global_position.direction_to(Global.player_pos)
+	new_bullet.position = spawn_pos
+	new_bullet.scale = Vector2(5,5)
+	return new_bullet
+
+func shoot():
+	var spawn_pos = enemy_projectile_image.global_position
+	get_parent().get_parent().get_node("EnemyProjectiles").add_child(create_bullet(spawn_pos))
+	
