eb8df329d945460dfa0d941aa8923939353d0d60
Kepalsbre <128705198+Kepalsbre@users.noreply.github.com>
2024-08-13 23:00:26 +0200

    added slinger shot sound and started razot

--- AbilityGame/scripts/razor_enemy.gd
+++ AbilityGame/scripts/razor_enemy.gd
@@ -1,31 +1,26 @@
 extends CharacterBody2D
 
+
 enum state {idle, seek, attack}
 @export var current_state: state = state.idle
-@export var wake_lenght := 400
-
-@export var knockback: float = 1.0:
+@export var wake_lenght := 800
+@export var knockback: float = 5:
 	get:
 		return knockback * Global.enemy_knockback_multiplier
-@export var damage: float = 12.0:
+@export var damage: float = 20.0:
 	get:
 		return damage * Global.enemy_dmg_multiplier
 
-@onready var animations = $Animations
+
 @onready var idle_image = $IdleImage
 @onready var offset_timer = $OffsetTimer
 @onready var health_component = $HealthComponent
 @onready var navigation_agent_2d = $NavigationAgent2D
-@onready var animation_player = $AnimationPlayer
-const ENEMY_BULLET = preload("res://scenes/enemy_bullet.tscn")
-@onready var enemy_projectile_image = $EnemyProjectileImage
-@onready var hp_bar_component = $HpBarComponent
 @onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 
-
 var player_pos
-var speed : int = randi_range(300, 380)
+var speed : int = randi_range(400, 480)
 var offset : Vector2
 var knockback_received := Vector2.ZERO
 var awake := false
@@ -34,30 +29,23 @@ var knock_frames := 0
 signal wake_up
 
 func _ready():
-	animations.visible = false
+
 	for enemy in get_parent().get_children():
 		enemy.wake_up.connect(_on_wake_up)
 	health_component.max_health *= Global.enemy_hp_multiplier
 	health_component.health *= Global.enemy_hp_multiplier
-	hp_bar_component.top_level = true
-	hp_bar_component.visible = false
 	navigation_agent_2d.max_speed = speed
 	
 	
-func _process(_delta):
-	hp_bar_component.position = global_position + Vector2(-61, 85)
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	player_pos = Global.player_pos
 	match current_state:
 		state.idle:
 			if global_position.distance_to(player_pos) < wake_lenght \
 			or health_component.max_health != health_component.health \
 			or awake:
-				animations.play("follow")
 				current_state = state.seek
 				idle_image.visible = false
-				animations.visible = true
 				offset = randoffset()
 				offset_timer.start()
 				wake_up.emit()
@@ -69,24 +57,19 @@ func _physics_process(delta):
 			var new_velocity = current_agent_position.direction_to(next_agent_position) * speed + knockback_received
 			navigation_agent_2d.set_velocity(new_velocity)
 			
-		state.attack:
-			velocity = Vector2.ZERO
-			if position.distance_to(player_pos) > 1100:
-				animations.play("follow")
-				current_state = state.seek
-				animation_player.stop()
-	
 			
-	# knockback stuff
+		state.attack:
+			velocity = (player_pos - position)
+			velocity = velocity.normalized() * speed
+			if position.distance_to(player_pos) > 200:
+				current_state = state.seek
+	
 	if knock_frames != 0:
 		knock_frames -= 1
 		velocity = knockback_received
 	else:
 		knockback_received = Vector2.ZERO
-	if current_state != state.idle:
-		look_at(player_pos)
-		
-	move_and_collide(velocity*delta)
+	move_and_slide()
 	
 
 func randoffset():
@@ -94,50 +77,27 @@ func randoffset():
 	var distance = randi_range(30,200)
 	return Vector2(distance * cos(angle), distance * sin(angle)) # polar to cartesian
 
+
 func _on_offset_timer_timeout():
 	offset = randoffset()
+
+
+func _on_hitbox_component_area_entered(area):
+	if area is HitboxComponent:
+		var hitbox : HitboxComponent = area
+		var attack = Attack.new()
+		attack.attack_damage = damage
+		attack.knockback_force = knockback
+		attack.attack_position = global_position
+		hitbox.damage(attack)
 
 func knock_back(knockforce, knock_pos):
 	knockback_received = (global_position - knock_pos) 
 	knockback_received = knockback_received.normalized() * (speed / 4) * knockforce
+
 	knock_frames = 20
+	
 	
 func _on_wake_up():
 	await get_tree().create_timer(randf_range(1,15)).timeout
 	awake = true
-	
-func _on_navigation_agent_2d_target_reached():
-	animations.play("attack") 
-	animation_player.play("shoot")
-	current_state = state.attack
-
-
-func create_bullet(spawn_pos):
-	var new_bullet = ENEMY_BULLET.instantiate()
-	new_bullet.damage = damage
-	new_bullet.knockback = knockback
-	new_bullet.velocity = global_position.direction_to(Global.player_pos)
-	new_bullet.position = spawn_pos
-	new_bullet.scale = Vector2(5,5)
-	return new_bullet
-
-func shoot():
-	var spawn_pos = enemy_projectile_image.global_position
-	get_parent().get_parent().get_node("EnemyProjectiles").add_child(create_bullet(spawn_pos))
-	
-
-
-func _on_navigation_agent_2d_velocity_computed(safe_velocity):
-	velocity = safe_velocity
-
-
-func _on_health_component_killed():
-	hide()
-	$HitboxComponent/Hitbox.set_deferred("disabled", true)
-	$Collision.set_deferred("disabled", true)
-	await audio_stream_player_2d.finished
-	queue_free()
-
-
-func _on_health_component_hitted():
-	audio_stream_player_2d.play()
