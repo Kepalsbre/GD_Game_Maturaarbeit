81fe78b63e5e209c832b4351f52d4f72906e2620
Kepalsbre <128705198+Kepalsbre@users.noreply.github.com>
2024-09-27 01:01:11 +0200

    started creating shotgun ability

--- AbilityGame/scripts/Global.gd
+++ AbilityGame/scripts/Global.gd
@@ -16,5 +16,6 @@ var ability_dict = {
 	"Rock Throw" : preload("res://scenes/rock_throw.tscn"),
 	"Spikebomb" : preload("res://scenes/spike_bomb_spawner.tscn"),
 	"Duplicate" : preload("res://scenes/duplicate.tscn"),
-	"True Form" : preload("res://scenes/true_form.tscn")
+	"True Form" : preload("res://scenes/true_form.tscn"),
+	"Shotgun" : preload("res://scenes/shotgun.tscn")
 }
